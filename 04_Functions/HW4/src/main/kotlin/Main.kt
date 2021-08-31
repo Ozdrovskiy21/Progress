6983e37a6cf42dddf1a0c4fee221cfa2238224d4
Ozdrovskiy21 <ozdrosvkiyvlad21@gmail.com>
2021-08-31 14:11:47 +0600

    Переделанное

--- 04_Functions/HW4/src/main/kotlin/Main.kt
+++ 04_Functions/HW4/src/main/kotlin/Main.kt
@@ -1,8 +1,8 @@
 fun main() {
-    println("Итог первой части: "+transcript1Part("F2p)v\"y233{0->c}ttelciFc"))
-    println("Итог второй части: "+transcript2Part("F2p)v\"y233{0->c}ttelciFc"))
-    println(transcript1Part("F2p)v\"y233{0->c}ttelciFc")+transcript2Part("F2p)v\"y233{0->c}ttelciFc"))
-    //вопросы с разделением текста простым путём, пока оставил такой вывод.
+    var string: String = "F2p)v\"y233{0->c}ttelciFc"
+    println(transcript1Part(string.substring(0, string.length/2)) +
+            transcript2Part(string.substring(string.length/2, string.length)))
+    
 }
 
 
@@ -10,28 +10,27 @@ fun transcript1Part(sourceString: String): String {
     var shiftedString = sourceString.map {char -> char + 1}.joinToString("")
     println("2.5 "+shiftedString+ " Сдвиг каждого символа вправо на 1")
 
-    shiftedString=  shiftedString.replace('5','S', true)
-    println("2.4 "+shiftedString+ " Замена цифры 5 на символ S.")
+    shiftedString=  shiftedString.replace('5','s')
+    println("2.4 "+shiftedString+ " Замена цифры 5 на символ s.")
 
-    shiftedString=  shiftedString.replace('4','U', true)
-    println("2.3 "+shiftedString+ " Замена цифры 4 на символ U.")
+    shiftedString=  shiftedString.replace('4','u')
+    println("2.3 "+shiftedString+ " Замена цифры 4 на символ u.")
 
     shiftedString = sourceString.map {char -> char - 3}.joinToString("")
     println("2.2 "+shiftedString+" Сдвиг каждого символа влево на 3")
 
-    shiftedString=  shiftedString.replace('0','o', true)
+    shiftedString=  shiftedString.replace('0','o')
     println("2.1 "+shiftedString+ " Замена цифры 0 на символ o.")
 
     return shiftedString
 }
 
 fun transcript2Part(sourceString: String): String {
-    //3.3. Развернуть получившуюся строку. - не понял данный пункт, ещё более непонятно, что делать в обратном направлении.
-    var shiftedString = sourceString.map {char -> char - 4}.joinToString("")
-    println("3.2 "+shiftedString+" Сдвиг каждого символа влево на 4")
+    var shiftedString = sourceString.reversed()
 
-    shiftedString=  shiftedString.replace(' ','_', true)
-    println("2.1 "+shiftedString+ " Замена пробелов на символ _.")
+    shiftedString = shiftedString.map {char -> char - 4}.joinToString("")
+
+    shiftedString=  shiftedString.replace('_',' ')
 
     return shiftedString
 }
